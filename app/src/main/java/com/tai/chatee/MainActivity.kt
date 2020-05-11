fc2d861d277d1b45772ce19ca7f61392f876fef2
neilquizon <neil.quizon@gmail.com>
2020-05-11 10:13:00 -0700

    Chatee

---
+++ app/src/main/java/com/tai/chatee/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.neil.chatee
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import kotlinx.android.synthetic.main.activity_auth.*
+import kotlinx.android.synthetic.main.activity_auth.auth_button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        login_button.setOnClickListener {
+            startActivity(AuthActivity.newIntent(AuthMode.Login, this))
+        }
+
+        register_button.setOnClickListener {
+            startActivity(AuthActivity.newIntent(AuthMode.Register, this))
+        }
+    }
+}
+
+sealed class AuthMode() : Parcelable {
+    @Parcelize
+    object Login : AuthMode()
+
+    @Parcelize
+    object Register : AuthMode()
+}
